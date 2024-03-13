fda978fb29b13b68a445cc57dd0a028320fa26ae
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-03-13 18:50:58 +0700

    create parallax

---
+++ parallax.js
@@ -0,0 +1,8 @@
+alert("Welcome to My Website");
+
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+    let offset = window.scrollY;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
